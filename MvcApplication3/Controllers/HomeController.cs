83aefeb4fad181d4b0deb2af95b1e041a2c43fe6
Moshe Levi <mo6709@gmail.com>
2018-03-06 17:34:57 -0500

    first commit

---
+++ MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string MyString = ViewBag.Message =  "Please enter a location";
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendRestRequest(decimal latitude, decimal longitude)
+        {
+            
+                var stopwatch = new System.Diagnostics.Stopwatch();
+                
+                 
+               
+                    // fetch data (as JSON string)
+                    string uri = ""; 
+                           uri = uri + "https://maps.googleapis.com/maps/api/timezone/json?location="
+                                 + latitude.ToString()
+                                 + ","
+                                 + longitude.ToString()
+                                 + "&timestamp=1331161200"
+                                 + "&key=AIzaSyDq7uE5UVW_zceDNiLgCj8vxUA5ku9jg6g";
+
+                           
+                    var url = new Uri(uri);
+                    var client = new System.Net.WebClient();
+                    var json = client.DownloadString(url);
+
+                    // deserialize JSON into objects
+                    var serializer = new JavaScriptSerializer();
+                    var data = serializer.Deserialize<Models.Data>(json);
+          
+                    // use the objects
+                    decimal DstOffset = data.dstOffset;
+                    decimal RawOffset = data.rawOffset;
+
+                    DateTime theTime = HttpContext.Timestamp;
+                    
+                    ViewBag.jsonDstOffset = data.dstOffset;
+                    ViewBag.jsonRawOffset = data.rawOffset;
+                    ViewBag.jsonStatus = data.status;
+                    ViewBag.jsonTimeZoneId = data.timeZoneId;
+                    ViewBag.jsonTimeZoneName = data.timeZoneName;
+                    ViewBag.thetime = theTime;
+                    ViewBag.latitudeNumber = "";
+                    ViewBag.longitudeNumber = "";
+                   
+            return View();
+        }
+
+    }
+
+    
+}
---
+++ MvcApplication3/Models/JsonObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace MvcApplication3.Models
+{
+    [DataContract]
+    public class Data
+    {
+        [DataMember]
+        public decimal dstOffset { get; set; }
+        [DataMember]
+        public decimal rawOffset { get; set; }
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public string timeZoneId { get; set; }
+        [DataMember]
+        public string timeZoneName { get; set; }
+    }
+
+    public class JsonObject : IHttpModule
+    {
+        /// <summary>
+        /// You will need to configure this module in the Web.config file of your
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            //clean-up code here.
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Below is an example of how you can handle LogRequest event and provide 
+            // custom logging implementation for it
+            context.LogRequest += new EventHandler(OnLogRequest);
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+    }
+}
